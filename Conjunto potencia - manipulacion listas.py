0ff80146b15203ac08d7294f97731fa1a67f79e0
dwn84 <50523550+dwn84@users.noreply.github.com>
2022-08-04 09:55:03 -0500

    Create Conjunto potencia - manipulacion listas.py

---
+++ Conjunto potencia - manipulacion listas.py
@@ -0,0 +1,55 @@
+#Recorderis de manejo de listas en Python
+datos = [1,2,3,4,5,6,7]#Lista circular doble
+print(datos[0])
+print(datos[5])
+print(datos[:])
+print(datos[-1])
+print(datos[-2])
+print(datos[:-1])
+print(datos[-1:])
+datos + [9]
+
+def validarX(n):
+    if(n%2==0):
+        return True
+    else:
+        return False
+
+resultado = filter(validarX,datos)  
+print(list(resultado))
+print(datos)
+
+#Listas de compresion
+
+for x in datos:
+    print(x)
+for x in range(10):
+    print(x)
+
+c = [2 ** x for x in datos if(x%2==0)]
+print(c)
+
+#[2, 4, 8, 16, 32, 64, 128]
+#[1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
+
+
+lista = [numero for numero in
+	[numero**2 for numero in range(0,11)]
+		if numero % 2 == 0]
+
+print(lista)
+
+
+
+mis_daticos= ['Lechuga','tomate','cebolla']
+
+
+def conjunto_potencia(c):
+    if(len(c)==0):
+        return [[]]
+    else:
+        ultimo = conjunto_potencia(c[:-1])
+        return ultimo + [listaC + [c[-1]] for listaC in ultimo]
+
+print(conjunto_potencia(mis_daticos))    
+#[[], ['Lechuga'], ['tomate'], ['Lechuga', 'tomate'], ['cebolla'], ['Lechuga', 'cebolla'], ['tomate', 'cebolla'], ['Lechuga', 'tomate', 'cebolla']]
